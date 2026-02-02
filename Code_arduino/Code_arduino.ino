9b0f9098df0adecb103f960b009814d6036b2f3b
huynguyen6923-hub <huynguyen6923@gmail.com>
2026-02-02 20:57:26 +0700

    init project

--- Code_arduino/Code_arduino.ino
+++ Code_arduino/Code_arduino.ino
@@ -1,43 +1,31 @@
 #include <Adafruit_PWMServoDriver.h>
 
 // Động cơ phía trước bên trái
-#define ENCA_FL 2 // Interrupt pin
-#define ENCB_FL 51
 #define PWM_FL 13
 #define IN2_FL 23
 #define IN1_FL 22
 
 // Động cơ giữa bên trái
-#define ENCA_ML 20 // Interrupt pin
-#define ENCB_ML 23
 #define PWM_ML 11
 #define IN2_ML 29
 #define IN1_ML 28
 
 // Động cơ phía sau bên trái
-#define ENCA_BL 19 // Interrupt pin
-#define ENCB_BL 47
 #define PWM_BL 9
 #define IN2_BL 51
 #define IN1_BL 50
 
 // Động cơ phía trước bên phải
-#define ENCA_FR 3 // Interrupt pin
-#define ENCB_FR 48
 #define PWM_FR 12
 #define IN2_FR 24
 #define IN1_FR 25
 
 // Động cơ giữa bên phải
-#define ENCA_MR 18 // Interrupt pin
-#define ENCB_MR 46
 #define PWM_MR 10
 #define IN2_MR 30
 #define IN1_MR 31
 
 // Động cơ phía sau bên phải
-#define ENCA_BR 21 // Interrupt pin
-#define ENCB_BR 45
 #define PWM_BR 8
 #define IN2_BR 52
 #define IN1_BR 53
@@ -56,50 +44,38 @@ int start_angle = 90;
 // Khai báo PCA9685
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
 
-#define SERVOMIN  120  // Pulse min cho 0 độ (khoảng 1ms) 
-#define SERVOMAX  600  // Pulse max cho 180 độ (khoảng 2ms) 
+#define SERVOMIN  150  // Giá trị PWM cho 0 độ
+#define SERVOMAX  600  // Giá trị PWM cho 180 độ
 
 void setup() {
   Serial.begin(9600);
 
   // Thiết lập cho phía trước bên trái
-  pinMode(ENCA_FL, INPUT);
-  pinMode(ENCB_FL, INPUT);
   pinMode(PWM_FL, OUTPUT);
   pinMode(IN1_FL, OUTPUT);
   pinMode(IN2_FL, OUTPUT);
   
   // Thiết lập cho giữa bên trái
-  pinMode(ENCA_ML, INPUT);
-  pinMode(ENCB_ML, INPUT);
   pinMode(PWM_ML, OUTPUT);
   pinMode(IN1_ML, OUTPUT);
   pinMode(IN2_ML, OUTPUT);
 
   // Thiết lập cho phía sau bên trái
-  pinMode(ENCA_BL, INPUT);
-  pinMode(ENCB_BL, INPUT);
   pinMode(PWM_BL, OUTPUT);
   pinMode(IN1_BL, OUTPUT);
   pinMode(IN2_BL, OUTPUT);
 
   // Thiết lập cho phía trước bên phải
-  pinMode(ENCA_FR, INPUT);
-  pinMode(ENCB_FR, INPUT);
   pinMode(PWM_FR, OUTPUT);
   pinMode(IN1_FR, OUTPUT);
   pinMode(IN2_FR, OUTPUT);
 
   // Thiết lập cho giữa bên phải
-  pinMode(ENCA_MR, INPUT);
-  pinMode(ENCB_MR, INPUT);
   pinMode(PWM_MR, OUTPUT);
   pinMode(IN1_MR, OUTPUT);
   pinMode(IN2_MR, OUTPUT);
 
   // Thiết lập cho phía sau bên phải
-  pinMode(ENCA_BR, INPUT);
-  pinMode(ENCB_BR, INPUT);
   pinMode(PWM_BR, OUTPUT);
   pinMode(IN1_BR, OUTPUT);
   pinMode(IN2_BR, OUTPUT);
@@ -222,15 +198,6 @@ void diagonalLeft() {
 }
 // Lệnh O
 void bigRight() {
-  setServoAngle(0, 120 + fl_ofst);
-  setServoAngle(1, 90 + ml_ofst);
-  setServoAngle(2, 60 + bl_ofst);
-  setServoAngle(3, 120 + fr_ofst);
-  setServoAngle(4, 90 + mr_ofst);
-  setServoAngle(5, 60 + br_ofst);
-}
-// Lệnh L
-void smallRight() {
   setServoAngle(0, 110 + fl_ofst);
   setServoAngle(1, 90 + ml_ofst);
   setServoAngle(2, 70 + bl_ofst);
@@ -238,23 +205,32 @@ void smallRight() {
   setServoAngle(4, 90 + mr_ofst);
   setServoAngle(5, 70 + br_ofst);
 }
+// Lệnh L
+void smallRight() {
+  setServoAngle(0, 100 + fl_ofst);
+  setServoAngle(1, 90 + ml_ofst);
+  setServoAngle(2, 80 + bl_ofst);
+  setServoAngle(3, 100 + fr_ofst);
+  setServoAngle(4, 90 + mr_ofst);
+  setServoAngle(5, 80 + br_ofst);
+}
 // Lệnh U
 void bigLeft() {
-  setServoAngle(0, 60 + fl_ofst);
-  setServoAngle(1, 90 + ml_ofst);
-  setServoAngle(2, 120 + bl_ofst);
-  setServoAngle(3, 60 + fr_ofst);
-  setServoAngle(4, 90 + mr_ofst);
-  setServoAngle(5, 120 + br_ofst);
-}
-// Lệnh J
-void smallLeft() {
   setServoAngle(0, 70 + fl_ofst);
   setServoAngle(1, 90 + ml_ofst);
   setServoAngle(2, 110 + bl_ofst);
   setServoAngle(3, 70 + fr_ofst);
   setServoAngle(4, 90 + mr_ofst);
   setServoAngle(5, 110 + br_ofst);
+}
+// Lệnh J
+void smallLeft() {
+  setServoAngle(0, 80 + fl_ofst);
+  setServoAngle(1, 90 + ml_ofst);
+  setServoAngle(2, 100 + bl_ofst);
+  setServoAngle(3, 80 + fr_ofst);
+  setServoAngle(4, 90 + mr_ofst);
+  setServoAngle(5, 100 + br_ofst);
 }
 // Lệnh K(Dừng servo)
 void center() {
@@ -267,28 +243,51 @@ void center() {
 }
 // Lệnh E(Quay tròn về bên phải)
 void rotateright() {
-  setServoAngle(0, 120 + fl_ofst);
+  setServoAngle(0, 160 + fl_ofst);
   setServoAngle(1, 90 + ml_ofst);
-  setServoAngle(2, 60 + bl_ofst);
-  setServoAngle(3, 60 + fr_ofst);
+  setServoAngle(2, 20 + bl_ofst);
+  setServoAngle(3, 20 + fr_ofst);
   setServoAngle(4, 90 + mr_ofst);
-  setServoAngle(5, 120 + br_ofst);
+  setServoAngle(5, 160 + br_ofst);
   
-  delay(200);
+  delay(500);
+
+  setMotor(1, 255, PWM_FL, IN1_FL, IN2_FL);
+  setMotor(1, 127, PWM_ML, IN1_ML, IN2_ML);
+  setMotor(1, 255, PWM_BL, IN1_BL, IN2_BL);
+  setMotor(-1, 255, PWM_FR, IN1_FR, IN2_FR);
+  setMotor(-1, 127, PWM_MR, IN1_MR, IN2_MR);
+  setMotor(-1, 255, PWM_BR, IN1_BR, IN2_BR);
 }
 // Lệnh Q(Quay tròn về bên trái)
 void rotateleft() {
-  setServoAngle(0, 120 + fl_ofst);
+  setServoAngle(0, 160 + fl_ofst);
   setServoAngle(1, 90 + ml_ofst);
-  setServoAngle(2, 60 + bl_ofst);
-  setServoAngle(3, 60 + fr_ofst);
+  setServoAngle(2, 20 + bl_ofst);
+  setServoAngle(3, 20 + fr_ofst);
   setServoAngle(4, 90 + mr_ofst);
-  setServoAngle(5, 120 + br_ofst);
+  setServoAngle(5, 160 + br_ofst);
   
-  delay(200);
+  delay(500);
+
+  setMotor(-1, 255, PWM_FL, IN1_FL, IN2_FL);
+  setMotor(-1, 127, PWM_ML, IN1_ML, IN2_ML);
+  setMotor(-1, 255, PWM_BL, IN1_BL, IN2_BL);
+  setMotor(1, 255, PWM_FR, IN1_FR, IN2_FR);
+  setMotor(1, 127, PWM_MR, IN1_MR, IN2_MR);
+  setMotor(1, 255, PWM_BR, IN1_BR, IN2_BR);
 }
 // Lệnh D(Xoay góc 180 đi sang phải)
 void crabright() {
+  setMotor(-1, 255, PWM_FL, IN1_FL, IN2_FL);
+  setMotor(-1, 255, PWM_ML, IN1_ML, IN2_ML);
+  setMotor(-1, 255, PWM_BL, IN1_BL, IN2_BL);
+  setMotor(-1, 255, PWM_FR, IN1_FR, IN2_FR);
+  setMotor(-1, 255, PWM_MR, IN1_MR, IN2_MR);
+  setMotor(-1, 255, PWM_BR, IN1_BR, IN2_BR);
+
+  delay(500);
+
   setServoAngle(0, 180 + fl_ofst);
   setServoAngle(1, 180 + ml_ofst);
   setServoAngle(2, 180 + bl_ofst);
@@ -296,10 +295,18 @@ void crabright() {
   setServoAngle(4, 180 + mr_ofst);
   setServoAngle(5, 180 + br_ofst);
 
-  delay(200);
 }
 // Lệnh A(Xoay góc 0 đi sang trái)
 void crableft() {
+  setMotor(1, 255, PWM_FL, IN1_FL, IN2_FL);
+  setMotor(1, 255, PWM_ML, IN1_ML, IN2_ML);
+  setMotor(1, 255, PWM_BL, IN1_BL, IN2_BL);
+  setMotor(1, 255, PWM_FR, IN1_FR, IN2_FR);
+  setMotor(1, 255, PWM_MR, IN1_MR, IN2_MR);
+  setMotor(1, 255, PWM_BR, IN1_BR, IN2_BR);
+  
+  delay(500);
+
   setServoAngle(0, 0 + fl_ofst);
   setServoAngle(1, 0 + ml_ofst);
   setServoAngle(2, 0 + bl_ofst);
@@ -307,7 +314,6 @@ void crableft() {
   setServoAngle(4, 0 + mr_ofst);
   setServoAngle(5, 0 + br_ofst);
 
-  delay(200);
 }
 // Lệnh H(dừng động cơ)
 void setMotor(int dir, int pwmVal, int pwm, int in1, int in2) {
@@ -325,7 +331,7 @@ void setMotor(int dir, int pwmVal, int pwm, int in1, int in2) {
 }
 // Hàm điều khiển xung PWM cho servo
 void setServoAngle(int channel, int angle) {
-  angle = constrain(angle, 0, 180); // bảo vệ cực an toàn
+  angle = constrain(angle, 0, 180); // Bảo vệ cực an toàn để tránh góc ngoài khoảng 0-180
   int pulse = map(angle, 0, 180, SERVOMIN, SERVOMAX);
   pwm.setPWM(channel, 0, pulse);
 }
